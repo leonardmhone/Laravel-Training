ff0885d001aed5bb63ab49a995e4d35eaecb2c99
Leon JJ Mh1 <info@itecicthub.com>
2023-01-25 02:10:29 +0200

    More web.php changes

--- Webdev/routes/web.php
+++ Webdev/routes/web.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-   /* return view('welcome');*/
-   return "Hello World"
+Route::get('/about', function () {
+    return view('pages.about');
 });
